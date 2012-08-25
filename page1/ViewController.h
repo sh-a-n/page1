1fed266453f6569b76425dc2e9a9066f3c03c217
Aleksey <sh-a-n@yandex.ru>
2012-08-26 03:25:31 +0400

    first commit

--- page1/ViewController.h
+++ page1/ViewController.h
@@ -7,7 +7,42 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SoundPlayer.h"
+
+@class FilmViewController;
 
 @interface ViewController : UIViewController
 
-@end
+@property (strong, nonatomic) FilmViewController *filmViewController;
+
+@property (nonatomic, retain) NSArray *files;
+
+@property (weak, nonatomic) IBOutlet UIImageView *timerBack;
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *xNavButton;
+
+@property (weak, nonatomic) IBOutlet UIButton *playButton;
+
+@property (weak, nonatomic) IBOutlet UIButton *fast_forwardButton;
+
+@property (weak, nonatomic) IBOutlet UIButton *rewardButton;
+
+@property (weak, nonatomic) IBOutlet UINavigationBar *navigationBar;
+
+@property (weak, nonatomic) IBOutlet UIToolbar *toolBar;
+
+@property (weak, nonatomic) IBOutlet UIProgressView *progressBar;
+
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *nextPageButton;
+
+@property (weak, nonatomic) IBOutlet UIImageView *background;
+
+@property (nonatomic, retain) NSTimer * myTimer;
+
+- (IBAction)playButtonTap:(id)sender;
+
+- (IBAction)xselector:(id)sender;
+
+- (IBAction)nextPageSelector:(id)sender;@end
